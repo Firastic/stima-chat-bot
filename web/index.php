d20b2f5968d2ab453d8ee276a2dd855b4586a1eb
Firastic <adyaksa.wisanggeni@gmail.com>
2019-04-18 00:24:58 +0700

    Fix response

--- web/index.php
+++ web/index.php
@@ -71,12 +71,12 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                     // or we can use replyMessage() instead to send reply message
                     // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
                     // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
-    
-                    return $response->withJson($result->getJSONDecodedBody(), $result->getHTTPStatus());
                 }
             
             }
         }
+        $response->write('OK');
+        return $response;
     }
 });
 
