b8d79eaf3d283530d135b46db9c6a7d920284e11
Firastic <adyaksa.wisanggeni@gmail.com>
2019-04-21 23:32:14 +0700

    Test imageMessage

--- web/index.php
+++ web/index.php
@@ -92,7 +92,7 @@ $app->post('/webhook', function ($request, $response) use ($bot, $pass_signature
                     $replyMessage = new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
                     $replyMessage.add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($reply_text));
                     //$replyMessage.add(new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder('https://stima-chat-bot.herokuapp.com/assets/playful.jpeg', 'https://stima-chat-bot.herokuapp.com/assets/playful.jpeg'));
-                    $result = $bot->replyMessage($event['replyToken'], $replyMessage);
+                    $result = $bot->replyMessage($event['replyToken'], new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($reply_text));
                 }
             }
         }
